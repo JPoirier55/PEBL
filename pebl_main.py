ac3a95a2acb5321a5349f82035106e666ac9d3a5
JPoirier55 <jake.poirier55@gmail.com>
2019-05-20 16:44:23 -0400

    Updates to running as a script

--- pebl_main.py
+++ pebl_main.py
@@ -2,14 +2,21 @@ from pyModbusTCP.client import ModbusClient
 import time
 import json
 import csv
+import xlsxwriter
 
 CLOSE_MODBUS = False
 
+def csv_json_addresses():
+    f = open('Address List.csv', 'r')
+    f.readline()
+    reader = csv.DictReader(f, fieldnames=("name", "address", "system"))
+    out = json.dumps([row for row in reader])
+    return json.loads(out)
 
 def csv_json_alarms(system):
     f = open('Alarm_List_' + system + '.csv', 'r')
     f.readline()
-    reader = csv.DictReader(f, fieldnames=("alarm", "address", "type"))
+    reader = csv.DictReader(f, fieldnames=("alarm", "address", "type", "trigger"))
     out = json.dumps([row for row in reader])
     return json.loads(out)
 
@@ -39,6 +46,30 @@ class Pebl:
         f.write(json.dumps(return_values))
         f.close()
 
+    def run_manual(self):
+        return_values = []
+        data_points = csv_json_alarms(self.system)
+        for data_point in data_points:
+            print(data_point)
+            if data_point['type'] == 'COIL':
+                value = self.c.read_coils(int(data_point['address']))
+                name = data_point['alarm']
+                if data_point['trigger'] == 'NC':
+                    if not value[0]:
+                        return_values.append({'name': name,
+                                              'value': 'OK'})
+                    else:
+                        return_values.append({'name': name,
+                                              'value': 'ALARM'})
+                elif data_point['trigger'] == 'NO':
+                    if value[0]:
+                        return_values.append({'name': name,
+                                              'value': 'ALARM'})
+                    else:
+                        return_values.append({'name': name,
+                                              'value': 'OK'})
+        return return_values
+
 
 def run_thread(address, system):
     pebl = Pebl(address, system)
@@ -47,8 +78,53 @@ def run_thread(address, system):
         print('running')
         time.sleep(5)
 
+
+def run_manual():
+    outputs = []
+    for system in csv_json_addresses():
+
+        pebl = Pebl('192.168.0.59', '7040')
+        outputs.append({'name': system['name'] + '-' + system['system'],
+                        'system': system['system'],
+                        'ip': system['address'],
+                        'alarms': pebl.run_manual()})
+        pebl.c.close()
+    print(outputs)
+    clean_outputs(outputs)
+
+
+def clean_outputs(outputs):
+    workbook = xlsxwriter.Workbook('Report.xlsx')
+    red_cell = workbook.add_format({'bold': True, 'font_color': 'red', 'bg_color': '#f4c8c8'})
+    green_cell = workbook.add_format({'bold': True, 'font_color': 'green', 'bg_color': '#dbf4c8'})
+    second_header_cell = workbook.add_format({'bold': True, 'font_size': 15, 'bg_color': '#e2e2e2'})
+    worksheet = workbook.add_worksheet()
+    for i in range(5):
+        worksheet.set_column(i, 0, 25)
+    index = 1
+    for dict_obj in outputs:
+        worksheet.write_row('A'+str(index), (dict_obj['name'], dict_obj['ip']), second_header_cell)
+        index += 1
+        for alarm in dict_obj['alarms']:
+            print(alarm)
+            if alarm['value'] == 'ALARM':
+                worksheet.write_row('A'+str(index), (str(alarm['name']), str(alarm['value'])), red_cell)
+            else:
+                worksheet.write_row('A' + str(index), (str(alarm['name']), str(alarm['value'])), green_cell)
+            index += 1
+    workbook.close()
+    # f = open('Report.csv', 'w')
+    # csv_writer = csv.writer(f, delimiter=',')
+    # csv_writer.writerow(('IP', 'SYSTEM', 'ALARMS', 'VALUE'))
+    # for dict_obj in outputs:
+    #     csv_writer.writerow((dict_obj['ip'], dict_obj['system']))
+    #     for alarm in dict_obj['alarms']:
+    #         print(alarm)
+    #         csv_writer.writerow(('', '', alarm['name'], alarm['value']))
+
 if __name__ == '__main__':
-    pebl = Pebl('192.168.0.59', '7040')
-    pebl.run()
+    # clean_outputs('sdf')
+
+    run_manual()
 
 
